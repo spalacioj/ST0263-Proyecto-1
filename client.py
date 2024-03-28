2cfad977fef26f7bd46f0c4adaa627b1921e2e0b
spalacioj <samu43221@gmail.com>
2024-03-28 01:52:59 -0500

    arreglando metodo de enviar archivos para usar cualquier tipo de archivos

--- client.py
+++ client.py
@@ -1,34 +1,42 @@
+import os
 import grpc
 import FileSharing_pb2
 import FileSharing_pb2_grpc
 
-def enviar_chunks_al_servidor(file_location):
+def Create(file_location, chunks, file_size):
     # Establecer conexión con el servidor gRPC
-    with grpc.insecure_channel('localhost:50051') as channel:
+    file_extension = os.path.splitext(file_location)[1]
+    fileName = os.path.splitext(os.path.basename(file_location))[0]
+    with grpc.insecure_channel('localhost:50051',options=[
+        ('grpc.max_send_message_length', 100 * 1024 * 1024),
+        ('grpc.max_receive_message_length', 100 * 1024 * 1024)
+    ]) as channel:
+        
         # Crear un stub para el servicio ArchivoService
-        stub = FileSharing_pb2_grpc.ArchivoServiceStub(channel)
-        fileSize = 2 * 1024 * 1024
+        stub = FileSharing_pb2_grpc.ArchivoServiceStub(channel) 
+        chunkSize = int(file_size // chunks) + 1 # Le sumamos un 1 porque para leer el archivo usamos valores exactos y asi el archivo no quedara incompleto
+        fileSize = chunkSize * 1024 * 1024
         # Abrir el archivo en modo lectura binaria
         with open(file_location, 'rb') as f:
-            contador = 1
+            contador = 0
             # Leer el primer chunk
             chunk = f.read(fileSize)
-            while True:
-                if not chunk:
-                    stub.ArchivoTerminado(FileSharing_pb2.Estado(terminado=True)) 
-                    break
+            while contador < chunks:
                 # Enviar el chunk al servidor
-                stub.EnviarChunk(FileSharing_pb2.ArchivoChunk(contenido=chunk))
+                fileInfo = f'{fileName} {contador} {file_extension}'
+                stub.EnviarChunk(FileSharing_pb2.ArchivoChunk(contenido=chunk,fileInfo=fileInfo))
                 print(f"Chunk {contador} enviado")
                 contador += 1
                 # Leer el siguiente chunk
                 chunk = f.read(fileSize)
-                
+            stub.ArchivoTerminado(FileSharing_pb2.Estado(terminado=True, fileExtension=file_extension))
             print("Todos los chunks han sido enviados")
 
+
+
 def main():
-    file_location = './testFiles/test.txt'  # Reemplazar con la ubicación del archivo
-    enviar_chunks_al_servidor(file_location)
+    file_location = './testFiles/image.jpg'  
+    Create(file_location,2,9.7)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -10,25 +10,34 @@ import google.protobuf.empty_pb2 as empty_pb2
 class ArchivoService(FileSharing_pb2_grpc.ArchivoServiceServicer):
     def __init__(self):
         self.contenido_archivo = b''
+        self.chunkDict = {}
 
     def EnviarChunk(self, request, context):
         self.contenido_archivo += request.contenido
-        print("Chunk recibido")
-
+        print(request.fileInfo)
+        fileParts = request.fileInfo.split()
+        fileName = fileParts[0] + fileParts[2]
+        fileName = f'{fileName}_c{fileParts[1]}'
+        self.chunkDict[fileName] = request.contenido
         return FileSharing_pb2.Respuesta(mensaje="Chunk recibido exitosamente")
     
     def ArchivoTerminado(self, request, context):
     
         Terminado = request.terminado
+        FileExtension = request.fileExtension
+        print(self.chunkDict)
         if Terminado:
-            with open('archivo_recibido.txt', 'wb') as f:
+            with open(f'archivo_recibido{FileExtension}', 'wb') as f:
                 f.write(self.contenido_archivo)
 
         print("Archivo guardado exitosamente")
         return empty_pb2.Empty()
 
 def iniciar_servidor():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),options=[
+        ('grpc.max_send_message_length', 100 * 1024 * 1024),
+        ('grpc.max_receive_message_length', 100 * 1024 * 1024)
+    ])
     FileSharing_pb2_grpc.add_ArchivoServiceServicer_to_server(ArchivoService(), server)
     server.add_insecure_port('[::]:50051')
     print("Service is running... ")
